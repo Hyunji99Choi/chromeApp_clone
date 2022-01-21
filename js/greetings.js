cd7b621d6b60ecd9e1d7be0740ed0ab5e6b375d0
Hyunji99Choi <tyg06229@gmail.com>
2022-01-21 17:45:13 +0900

    right and left and footer finish. but right essue

--- js/greetings.js
+++ js/greetings.js
@@ -5,18 +5,25 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 const USERNAME_KEY = "username";
 
+const APPEAR_AMN = "hello-appear__Anmation";
+
 function onLoginSubmit(event) {
   event.preventDefault();
-  loginForm.classList.add(HIDDEN_CLASSNAME);
-  const username = loginInput.value;
-  localStorage.setItem(USERNAME_KEY, username);
+  //이름 입력창 사라지기
+  loginForm.classList.add("sigin-disappear__Anmation");
+  setTimeout(() => {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
 
-  // greeting.innerText = "Hello " + username;
-  paintGreetings(username);
+    // greeting.innerText = "Hello " + username;
+    paintGreetings(username);
+  }, 800);
 }
 
 function paintGreetings(username) {
   greeting.innerText = `Hello ${username}`;
+  greeting.classList.add(APPEAR_AMN);
   greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
@@ -24,6 +31,7 @@ function paintGreetings(username) {
 const savedUsername = localStorage.getItem(USERNAME_KEY);
 
 if (savedUsername === null) {
+  greeting.classList.add(APPEAR_AMN);
   loginForm.classList.remove(HIDDEN_CLASSNAME);
   loginForm.addEventListener("submit", onLoginSubmit);
 } else {
--- js/weather.js
+++ js/weather.js
@@ -7,8 +7,13 @@ function onGeoOk(position) {
   fetch(url)
     .then((response) => response.json())
     .then((data) => {
-      const weather = document.querySelector("#weather span:first-child");
+      const weather = document.querySelector("#weather span:nth-child(2)");
       const city = document.querySelector("#weather span:last-child");
+      const icon = document.querySelector("#weather img");
+
+      const iconCode = data.weather[0].icon;
+      const iconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+      icon.src = iconUrl;
 
       city.innerText = data.name;
       weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
